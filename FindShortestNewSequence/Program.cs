39234191cccc1b2473eef5810608daac05a28820
username <email@email.com>
2016-02-26 15:00:57 +0200

    find shortest new sequence topcoder

---
+++ FindShortestNewSequence/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindShortestNewSequence
+{
+  public class Program
+  {
+    static void Main(string[] args)
+    {
+    }
+
+    public String findShortestNewSequence(String sequence)
+    {
+      Queue<string> que = new Queue<string>();
+      Enqueue(que, String.Empty);
+
+      while (true)
+      {
+        string test = que.Dequeue();
+        if (sequence.Contains(test))
+        {
+          Enqueue(que, test);
+        }
+        else
+        {
+          return test;
+        }
+      }
+    }
+
+    private void Enqueue(Queue<string> que, string baseStr)
+    {
+      que.Enqueue(baseStr + "A");
+      que.Enqueue(baseStr + "C");
+      que.Enqueue(baseStr + "G");
+      que.Enqueue(baseStr + "T");
+    }
+  }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FindShortestNewSequence;
+using System.Linq;
+
+namespace UnitTestProject1
+{
+  [TestClass]
+  public class UnitTest1
+  {
+    [TestMethod]
+    public void TestMethod1()
+    {
+
+      string[] allSequences = new string[] {
+      "AGGTCTA",
+"AGACGACGGAGAACGA",
+"A",
+"AAGATACACCGGCTTCGTG"
+};
+
+      string[][] allMissing = new string[][] {
+        new string[] {"AA", "AC" },
+        new string[] {"T" },
+        new string[] {"C" },
+            new string[] {"AAA","CAT" }
+      };
+
+      Program p = new Program();
+      int length = allSequences.Length;
+      for (int i = 0; i < length; i++)
+      {
+        string sequence = allSequences[i];
+
+        string actual = p.findShortestNewSequence(sequence);
+
+        string[] expected = allMissing[i];
+
+        TestEqual(expected, actual);
+      }
+
+    }
+
+    private void TestEqual(string[] expected, string actual)
+    {
+      Assert.IsTrue(expected.Intersect(new string[] { actual }).Count() != 0);
+    }
+  }
+}
